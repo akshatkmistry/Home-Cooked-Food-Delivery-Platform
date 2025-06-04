6d7b629b471a34356359a785f8625ecb37bc1ec6
the._.akx <akshatkmistry8@gmail.com>
2025-06-05 02:58:54 +0530

    Update App.sx for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './screens/Home.jsx';
 import Login from './screens/Login.jsx';
 import Signup from './screens/Signup.jsx';
+import ChefDashboard from './screens/ChefDashboard.jsx';
 import{BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -12,13 +13,13 @@ function App() {
   return (
     <CartProvider>
       <Router>
-        <div>
-          <Routes>
+        <div>          <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/login" element={<Login/>} />
             <Route exact path="/createuser" element={<Signup/>} />
             <Route exact path="/cart" element={<Cart/>} />
             <Route exact path="/myorder" element={<MyOrder/>} />
+            <Route exact path="/chef-dashboard" element={<ChefDashboard/>} />
           </Routes>
         </div>
       </Router>
